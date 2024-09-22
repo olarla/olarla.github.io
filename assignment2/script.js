2245631fdc821a190af29f1a91a6d2e1122dd3d0
olarla <s3926872@rmit.edu.vn>
2024-09-22 17:05:15 +1000

    sep22

--- assignment2/script.js
+++
@@ -1,7 +0,0 @@
-function playTrack(videoUrl) {
-  var playerContainer = document.getElementById("player-container");
-  var youtubePlayer = document.getElementById("youtube-player");
-
-  youtubePlayer.src = videoUrl;
-  playerContainer.classList.remove("hidden");
-}
